5614f00ba3b19363a5f1b092b684d07aba62ad5f
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-09-30 23:01:30 -0300

    Slots

---
+++ 0506/vue-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <modal-principal>
+      <template #header>
+        <h4>Carlos Garcia é o dev mais rico de Angola.</h4>
+      </template>
+      <button>Mudar</button>
+      <template #footer>
+        <h2>Footer</h2>
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal";
+
+export default {
+  name: "App",
+  components: {
+    ModalPrincipal,
+  },
+};
+</script>
+
+<style>
+</style>
