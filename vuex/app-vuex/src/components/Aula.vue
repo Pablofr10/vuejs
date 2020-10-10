98b62343f874e18b4b150eb0d1e10f81af9f59b3
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-10 19:24:56 -0300

    Criando states

---
+++ vuex/app-vuex/src/components/Aula.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ user }} Aula {{ aulasCompletas }}</p>
+    <p>{{ nome }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Aula",
+  data() {
+    return {
+      nome: "André",
+    };
+  },
+  computed: {
+    ...mapState(["user", "aulasCompletas"]),
+    nomeMaiusculo() {
+      return this.nome.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
