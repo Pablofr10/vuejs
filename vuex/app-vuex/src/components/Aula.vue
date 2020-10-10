fe74df5181089766b4a33fc694d269a3888f1880
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-10 20:47:02 -0300

    Adicionando Mutations

--- vuex/app-vuex/src/components/Aula.vue
+++ vuex/app-vuex/src/components/Aula.vue
@@ -1,24 +1,70 @@
 <template>
   <div>
-    <p>{{ user }} Aula {{ aulasCompletas }}</p>
+    <!-- <p>{{ user }} Aula {{ aulasCompletas }}</p>
     <p>{{ nome }}</p>
+    <button @click="handleClick">Modificar usuário</button>
+    <button @click="COMPLETAR_AULA">Completar Aula</button>
+    <input type="text" v-model="novoUser" /> -->
+
+    <ul>
+      <li v-for="aula in aulas" :key="aula.nome">
+        {{ aula.nome }} {{ aula.duracao }} horas
+        <button @click="ADICIONAR_AULA(aula)">Marcar como concluída</button>
+      </li>
+    </ul>
+    <div>
+      <h2>Aulas finalizadas {{ totalHoras }}</h2>
+      <ul>
+        <li
+          v-for="aulaFinalizada in aulasFinalizadas"
+          :key="aulaFinalizada.nome"
+        >
+          {{ aulaFinalizada.nome }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "Aula",
   data() {
     return {
       nome: "André",
+      novoUser: "",
+      aulas: [
+        {
+          nome: "HTML e CSS",
+          duracao: 15,
+        },
+        {
+          nome: "JAVASCRIPT",
+          duracao: 40,
+        },
+        {
+          nome: "C#",
+          duracao: 30,
+        },
+        {
+          nome: ".NET",
+          duracao: 40,
+        },
+      ],
     };
   },
   computed: {
-    ...mapState(["user", "aulasCompletas"]),
+    ...mapState(["user", "aulasCompletas", "aulasFinalizadas", "totalHoras"]),
     nomeMaiusculo() {
       return this.nome.toUpperCase();
+    },
+  },
+  methods: {
+    ...mapMutations(["CHANGE_USER", "COMPLETAR_AULA", "ADICIONAR_AULA"]),
+    handleClick() {
+      this.CHANGE_USER({ user: this.novoUser });
     },
   },
 };
